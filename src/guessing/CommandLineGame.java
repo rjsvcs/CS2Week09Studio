5a3b6548d67cf9da71307f1af01f4f09533ce726
rjsvcs <rjsvcs@rit.edu>
2019-03-15 19:22:05 -0400

    Added some comments to guessing game.

--- src/guessing/CommandLineGame.java
+++
@@ -1,9 +0,0 @@
-package guessing;
-
-public class CommandLineGame {
-    public static void main(String[] args) {
-        GuessingGame game = new GuessingGameImpl();
-        GamePlayer player = new GamePlayer(game);
-        player.playTheGame();
-    }
-}
