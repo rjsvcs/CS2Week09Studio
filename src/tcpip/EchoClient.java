d27cc17bad7759b656e702ba59bf1e93aa309adc
rjsvcs <rjsvcs@rit.edu>
2020-03-10 08:41:14 -0400

    changed scanner to buffered reader in echo example.

--- src/tcpip/EchoClient.java
+++ src/tcpip/EchoClient.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class EchoClient {
     public static void main(String[] args) throws IOException {
         System.out.println("Connecting to server...");
-        Socket sock = new Socket("localhost", 1337);
+        Socket sock = new Socket("localhost", 13377);
         System.out.println("Connected!");
         OutputStream outputStream = sock.getOutputStream();
         PrintWriter writer = new PrintWriter(outputStream);
--- src/tcpip/EchoServer.java
+++ src/tcpip/EchoServer.java
@@ -1,23 +1,21 @@
 package tcpip;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
-        ServerSocket server = new ServerSocket(1337);
+        ServerSocket server = new ServerSocket(13377);
         System.out.println("Waiting for client...");
         Socket client = server.accept();
         System.out.println("Connected!");
         // receive
         InputStream inputStream = client.getInputStream();
-        Scanner scanner = new Scanner(inputStream);
-        String line = scanner.nextLine();
+        InputStreamReader iReader = new InputStreamReader(inputStream);
+        BufferedReader reader = new BufferedReader(iReader);
+        String line = reader.readLine();
         System.out.println("received: " + line);
         // echo back
         OutputStream outputStream = client.getOutputStream();
